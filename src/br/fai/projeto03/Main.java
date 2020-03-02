e78090ffdd1c73a9b3aac38a3d0505c009c6bd07
Matuk <mateusmatuk@fai-mg.br>
2020-03-02 20:56:28 -0300

    primeiro commit

--- src/br/fai/projeto03/Main.java
+++ src/br/fai/projeto03/Main.java
@@ -1,5 +1,18 @@
 package br.fai.projeto03;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+	
+	Main app = new Main();
+	app.start();
+	
+
+}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+	}
 }
